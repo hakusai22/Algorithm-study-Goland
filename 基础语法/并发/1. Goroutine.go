fe6ac63187da1975794d282a846357702e980223
yinpeng <y960303802@163.com>
2022-09-05 18:29:03 +0800

    [add] 并发/分支

---
+++ 基础语法/并发/1. Goroutine.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go testgo() //使用关键字go调用函数或者方法 开启一个goroutine
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+	fmt.Println("main 函数结束")
+	time.Sleep(3000 * time.Millisecond) //加上休眠让主程序休眠3秒钟。
+
+}
+
+//自定义函数
+func testgo() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("测试goroutine", i)
+	}
+}
