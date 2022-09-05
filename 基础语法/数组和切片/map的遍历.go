bc309381af3158d14d95d7765783116d971fe23e
yinpeng <y960303802@163.com>
2022-09-05 14:03:33 +0800

    [add] 切片和数组/map/内置包

---
+++ 基础语法/数组和切片/map的遍历.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//map的遍历
+	//因为map是无序的 如果需要获取map中所有的键值对
+	//可以使用 for range
+	map1 := make(map[int]string)
+	map1[1] = "张无忌"
+	map1[2] = "张三丰"
+	map1[3] = "常遇春"
+	map1[4] = "胡青牛"
+
+	//遍历map
+	for key, val := range map1 {
+		fmt.Println(key, val)
+	}
+}
